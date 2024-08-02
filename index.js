f9e7e753267aa26c87ac790998da83ab67c3446a
mivoavid <miika.voutilainen@avidlyagency.com>
2024-08-02 14:59:31 +0300

    Changes to files

--- index.js
+++ index.js
@@ -26,48 +26,24 @@ app.get('/discs', async (req, res) => {
     }
 });
 
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+/*
+app.get('/update-cobj', async (req, res) => {
+   
+        res.render('updates', { title: 'Disc Golf Discs | Update / add' });   
+   
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
+app.post('/update-cobj', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            "disc_name": "Test",
+            "manufacturer": "Innova",
+            "speed": "13"
         }
     }
 
     const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const updateContact = `https://api.hubapi.com/crm/v3/objects/2-32818682/?idProperty=email`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -82,7 +58,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
